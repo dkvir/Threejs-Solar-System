c83d84d043027c64b3e5b4f939e61cbe6e90eacf
Dato Kvirikashvili <kvirika137@gmail.com>
2023-05-13 21:24:05 +0400

    👌 IMPROVE: play music

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,5 +1,31 @@
-const loaderDiv = document.querySelector('.loader')
+const audio = new Audio('/audio/Space-Oddity.mp3');
+
+const loaderDiv = document.querySelector('.loader');
+const musicDiv = document.querySelector('.music');
+const musicToggler = document.querySelector('.music-toggle');
+const musicText = document.querySelector('.music-text');
+let playBowie = false;
+
+musicDiv.addEventListener('click', ()=>{
+    playBowie = !playBowie;
+
+    if(playBowie){
+        musicText.textContent = 'Pause Bowie';
+        musicToggler.classList.remove('music-stopped')
+        audio.play();
+    } else{
+        musicText.textContent = 'Play Bowie';
+        musicToggler.classList.add('music-stopped')
+        audio.pause();
+    } 
+})
 
 setTimeout(() => {    
   loaderDiv.classList.remove('loading');
 }, 4500);
+
+window.addEventListener("load", (event) => {
+    setTimeout(() => {
+        document.documentElement.classList.add('loaded');
+    }, 1000);
+});
